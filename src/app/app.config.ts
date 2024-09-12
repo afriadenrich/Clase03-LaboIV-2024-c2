d0dc597836793178dcf96634e009da074e3f1c68
AgustinFrich <agustinfrich@gmail.com>
2024-09-12 12:23:34 -0300

    environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,20 +6,12 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'sala-de-juegos-friadenrich',
-        appId: '1:637721224566:web:f2719815edf87614f3c657',
-        storageBucket: 'sala-de-juegos-friadenrich.appspot.com',
-        apiKey: 'AIzaSyAUoDDry11fqAtEcFeV80KdfgN1T90AMnU',
-        authDomain: 'sala-de-juegos-friadenrich.firebaseapp.com',
-        messagingSenderId: '637721224566',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.configFirebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
